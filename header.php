88c419727b721aec139e12e68a6524de01b08519
hilalahmad321 <73179869+hilalahmad321@users.noreply.github.com>
2020-10-22 08:04:26 -0700

    update

--- header.php
+++ header.php
@@ -1,1 +1,24 @@
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/w3.css">
+    <link rel="stylesheet" href="css/index.css">
+    <title>HakTutorial</title>
+    <link rel="shortcut icon" href="img/favcion.png" type="img/png">
+</head>
+
+<body>
+    <div class="navbar">
+        <div class="leftnavbar">
+            <a href="index.html">HakTutorial</a>
+        </div>
+        <div class="rightnavbar">
+            <a href="index.html">Home</a>
+            <a href="#web">Web development</a>
+        </div>
+    </div>
