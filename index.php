b74bb4b688f4571ad71fd568c3f0c61c6c92f9fb
hilalahmad321 <73179869+hilalahmad321@users.noreply.github.com>
2020-10-22 08:00:59 -0700

    update

--- index.php
+++ index.php
@@ -30,19 +30,6 @@
         <a href="htmltutorial.html" class="btn w3-btn">Html tutorial</a>
         <a href="webprojects.html" class="btn w3-btn">Web projects</a>
     </section>
-    <section class="footer w3-center">
-        <div class="icon w3-center w3-padding">
-            <a href="https://web.facebook.com/HAKtutorialkhan" target="_blank" class="fa fa-facebook"></a>
-            <a href="https://www.instagram.com/hak_tutoiral/?hl=en" target="_blank" class="fa fa-instagram"></a>
-            <a href="https://twitter.com/Hilalah14107592" target="_blank" class="fa fa-twitter"></a>
-            <a href="https://www.youtube.com/channel/UCmfzjm_HUGy4HZw26_-BESQ" target="_blank"
-                class="fa fa-youtube"></a>
-            <a href="https://www.youtube.com/channel/UCR5SgDcdUMcAsj4_ozsNmsA" target="_blank"
-                class="fa fa-youtube"></a>
-         </div>
-        <h6 class="w3-text-white">Copyright @ by <a href="https://hilalahmad321.github.io/haktutorial.github.io/index.html">Hilal Ahmad</a></h6>
-    </section>
-    <script src="script/script.js"></script>
-</body>
+<?php include("footer.php");?>
 
 </html>
