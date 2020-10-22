d1e2f0daba71f94040cea3e6fab83fba8636ddf1
hilalahmad321 <73179869+hilalahmad321@users.noreply.github.com>
2020-10-22 08:03:53 -0700

    update

--- index.php
+++ index.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/w3.css">
-    <link rel="stylesheet" href="css/index.css">
-    <title>HakTutorial</title>
-    <link rel="shortcut icon" href="img/favcion.png" type="img/png">
-</head>
-
-<body>
-    <div class="navbar">
-        <div class="leftnavbar">
-            <a href="index.html">HakTutorial</a>
-        </div>
-        <div class="rightnavbar">
-            <a href="index.html">Home</a>
-            <a href="#web">Web development</a>
-        </div>
-    </div>
+<?php include("header.php");?>
     <section class="section w3-center">
         <h1 class="w3-margin w3-jumbo w3-text-white text">Welcome to my website</h1>
         <h2>1 am <span class="w3-text-yellow">Web developer</span></h2>
